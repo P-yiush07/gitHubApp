5e4912b4bdbd109f0be862fb18bdff89104cff1e
P-yiush07 <piyush062006@outlook.com>
2023-10-23 17:21:53 +0530

    fix: routing2

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import Layout from './Layout.jsx'
 import Body from './components/Body/Body.jsx'
 import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromChildren } from 'react-router-dom'
+import { Route, RouterProvider, createHashRouter, createRoutesFromChildren } from 'react-router-dom'
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromChildren(
     <Route path='/' element={<Layout/>}>
       <Route path='' element={<Body/>}/>
